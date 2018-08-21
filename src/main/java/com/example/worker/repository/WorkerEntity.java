9c2d8a2584a63f687cfea3bfa17c10540b154b3c
Moskala <michal.moskala@outlook.com>
2018-08-21 15:32:38 +0200

    experimental pom3

--- src/main/java/com/example/worker/repository/WorkerEntity.java
+++ src/main/java/com/example/worker/repository/WorkerEntity.java
@@ -1,8 +1,8 @@
-package com.example.worker.repository;
-
-import javax.persistence.*;
-
-@Entity
-@Table(name="workers")
-public class WorkerEntity {
-}
+//package com.example.worker.repository;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name="workers")
+//public class WorkerEntity {
+//}
